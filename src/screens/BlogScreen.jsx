ae58d993280999607d5d9f1493279913106a3992
phonglam <lamphong1507@gmail.com>
2025-02-27 19:10:33 +0700

    co

---
+++ src/screens/BlogScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function BlogScreen() {
+  return (
+    <View>
+      <Text>BlogScreen</Text>
+    </View>
+  );
+}
